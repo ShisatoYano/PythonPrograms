0b551ffe050887f5f37b4304875a22c4c54141a9
LAPTOP-V8HODJUI\shisa <shisatoyano@gmail.com>
2018-07-14 12:07:05 +0900

    translate comment from Japanese to English

--- ObjectSensingSimulator/ObjectSensingSimulator.py
+++ ObjectSensingSimulator/ObjectSensingSimulator.py
@@ -20,25 +20,25 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import RadioButtons, Slider
 import matplotlib.animation as anm
 
-# クラス定義
+# Class Definition
 class ObjectSensingSimulator(object):
-    # コンストラクタ
+    # Constructor
     def __init__(self, deltaTime_s):
-        # 刻み時間[s]の設定
+        # delta Time [s]
         self.deltaTime_s = deltaTime_s
 
-        # センサの検知範囲パラメータ定義
-        self.maxRange_mm    = 100000           # 最大検知可能距離[mm]
-        self.minRange_mm    = 5000            # 最小検知可能距離[mm]
-        self.leftAngle_deg  = 45              # 左側検知可能角度[deg]  
-        self.rightAngle_deg = -45             # 右側検知可能角度[deg]
+        # Sensor detection coverage parameter
+        self.maxRange_mm    = 100000 # Max detect range[mm]
+        self.minRange_mm    = 5000   # Min detect range[mm]
+        self.leftAngle_deg  = 45     # left side limit angle[deg]  
+        self.rightAngle_deg = -45    # right side limit angle[deg]
 
-        # 検知範囲を扇形で描画するための角度配列
+        # Detection angle array
         angleStep = 1
         leftAngleArray  = np.arange(0, self.leftAngle_deg + angleStep, angleStep)
         rightAngleArray = np.arange(0, self.rightAngle_deg - angleStep, -angleStep)
 
-        # 左側の境界線座標配列
+        # Left side boundary array
         leftBoundaryX = []
         leftBoundaryX.append([self.maxRange_mm * cos(np.deg2rad(angle)) for angle in leftAngleArray])
         leftBoundaryX = np.append(leftBoundaryX, 0.0)
@@ -47,7 +47,7 @@ class ObjectSensingSimulator(object):
         leftBoundaryY = np.append(leftBoundaryY, 0.0)
         self.leftScanBoundary = np.vstack((leftBoundaryX, leftBoundaryY))
 
-        # 右側の境界線座標配列
+        # Right side boundary array
         rightBoundaryX = []
         rightBoundaryX.append([self.maxRange_mm * cos(np.deg2rad(angle)) for angle in rightAngleArray])
         rightBoundaryX = np.append(rightBoundaryX, 0.0)
@@ -56,122 +56,118 @@ class ObjectSensingSimulator(object):
         rightBoundaryY = np.append(rightBoundaryY, 0.0)
         self.rightScanBoundary = np.vstack((rightBoundaryX, rightBoundaryY))
 
-        # ターゲットの状態方程式行列A
+        # State Equation Matrix A
         self.A = np.array([[1.0, 0.0, 0.0],
                            [0.0, 1.0, 0.0],
                            [0.0, 0.0, 1.0]])
         
-        # ターゲットの状態方程式行列B(制御入力)
+        # State Equation Matrix B(Control Input)
         self.B = np.zeros((3, 3))
 
-        # ターゲットの初期状態
-        truePosX0_mm = 120000 # 位置X座標[mm]
-        truePosY0_mm = 0      # 位置Y座標[mm]
-        trueYaw0_rad = 0      # 方位角[rad]
-        # 真値
+        # Initial state of target object
+        truePosX0_mm = 120000 # target position X[mm]
+        truePosY0_mm = 0      # target position Y[mm]
+        trueYaw0_rad = 0      # Yaw angle[rad]
+        # Ground truth
         self.trueTarget = np.array([[truePosX0_mm],
                                     [truePosY0_mm],
                                     [trueYaw0_rad]])
-        # 観測値
+        # Observation
         self.observedTarget = self.trueTarget
-        # 観測角度
+        # Observed angle
         self.observedAngle_rad = 0  
     
-    # 速度と角速度からターゲットの真値を計算
+    # Generate ground truth of target
     def GenerateTrueTarget(self, currentX, inputV, inputOmega):
-        """次のサイクルのターゲットの状態を計算
-           状態方程式: x(k+1) = A * x(k) + B * u(k)
-        引数:
-            currentX:現在のターゲットの状態x(k)
-            inputV:速度入力[m/s]
-            inputOmega:角速度入力[deg/s]
-        返り値:
-            nextX:次のサイクルのターゲットの状態x(k+1)
-            velocityX_mms:ターゲット速度のcos成分
-            velosityY_mms:ターゲット速度のsin成分
         """
-        # 制御入力
+           State equation: x(k+1) = A * x(k) + B * u(k)
+        Input:
+            currentX:   Current state x(k)
+            inputV:     Input velosity[m/s]
+            inputOmega: Input yaw rate[deg/s]
+        return:
+            nextX:         next state x(k+1)
+            velocityX_mms: cos component of velosity
+            velosityY_mms: sin component of velosity
+        """
+        # Control input
         self.B[0, 0] = inputV * 1000 # [mm/s]
         self.B[1, 1] = inputV * 1000 # [mm/s]
         self.B[2, 2] = np.deg2rad(inputOmega)
 
-        currentYaw_rad = currentX[2, 0] # 移動方位角[rad]
+        currentYaw_rad = currentX[2, 0] # Yaw angle[rad]
         u = np.array([[self.deltaTime_s * cos(currentYaw_rad)],
                       [self.deltaTime_s * sin(currentYaw_rad)],
                       [self.deltaTime_s]])
 
-        # 次のサイクルの状態    
+        # State of next cycle    
         nextX = (self.A @ currentX) + (self.B @ u)
         nextX[2, 0] = pi2pi(nextX[2, 0])
 
-        # ターゲット速度のcos成分
+        # cos component of velosity
         velocityX_mms = inputV * 1000 * cos(nextX[2, 0])
-        # ターゲット速度のsin成分
+        # sin component of velosity
         velocityY_mms = inputV * 1000 * sin(nextX[2, 0])
 
         return nextX, velocityX_mms, velocityY_mms
     
-    # 真値にガウスノイズを付加したターゲットの観測値を計算
+    # Generate Observation of target 
     def GenerateObservedTarget(self, trueTarget, velocityX_mms, velocityY_mms):
-        """ターゲットの観測状態を計算
-            距離と角度に対してそれぞれ5%のガウスノイズを付加
-            センサ座標系で計算する
-        引数:
-            trueTarget:ターゲットの真値[x, y, yaw]
-            velocityX_mms:ターゲット速度のcos成分[mm/s]
-            velocityY_mms:ターゲット速度のsin成分[mm/s]
-        返り値:
-            observedRange_mm:観測距離[mm]
-            observedAngle_rad:観測角度[rad]
-            rangeVelosity_mms:距離方向速度[mm/s]
-            angleVelosity_rads:角度方向速度[rad/s]
         """
-        # 真値
+            Each observed value is calculated on sensor coordinate
+            2% gaussian noise is add
+        Input:
+            trueTarget:    Ground truth[x, y, yaw]
+            velocityX_mms: cos component of velosity[mm/s]
+            velocityY_mms: sin component of velosity[mm/s]
+        Return:
+            observedRange_mm:   Observed range to target[mm]
+            observedAngle_rad:  Observed angle to target[rad]
+            rangeVelosity_mms:  Observed velosity on range direction[mm/s]
+            angleVelosity_rads: Observed velosity on turning direction[rad/s]
+        """
+        # Ground truth
         truePosX_mm = trueTarget[0, 0]
         truePosY_mm = trueTarget[1, 0]
         trueYaw_rad = trueTarget[2, 0]
         
-        # 観測距離を計算
-        # 2%のガウスノイズを付加
+        # Calculate observed range
+        # add 2% gaussian noise
         trueRange_mm = sqrt( (pow(truePosX_mm,2) + pow(truePosY_mm,2)) )
         observedRange_mm = trueRange_mm + np.random.normal(0, 0.02*trueRange_mm)
 
-        # 観測角度を計算
+        # Calculate observed angle on sensor coordinate
         trueAngle_rad = atan(truePosY_mm/truePosX_mm)
-        # 2%のガウスノイズを付加
+        # add 2% gaussian noise
         observedAngle_rad = trueAngle_rad + np.random.normal(0, 0.01) + np.random.normal(0, 0.02*abs(trueAngle_rad)) 
 
-        # ターゲット速度の極座標成分を計算
-        # 距離方向速度
+        # Calculate observed velosity on polar coodinate
+        # Range direction
         rangeVelosity_mms = velocityX_mms * cos(observedAngle_rad) + velocityY_mms * sin(observedAngle_rad)
-        # 角度方向速度
+        # Turning direction 
         angleVelosity_rads = (observedAngle_rad - self.observedAngle_rad)/self.deltaTime_s
         self.observedAngle_rad = observedAngle_rad
 
         return trueRange_mm, trueAngle_rad, observedRange_mm, observedAngle_rad, rangeVelosity_mms, angleVelosity_rads
     
-    # 検知範囲の内外判定をする関数
+    # Check inside or outside sensor detection area
     def CheckInOutSensorCoverage(self, targetRange_mm, targetAngle_rad):
-        """扇形の内外判定アルゴリズム
-        引数:
-            targetRange_mm:ターゲットの距離[mm]
-            targetAngle_rad:ターゲットの角度[rad]
-        返り値:
-            insideFlag:検知範囲内ならTrue, 逆ならFalse
         """
-        # ステップ1:検知可能距離を半径とする円の外か判定
+        Input:
+            targetRange_mm:  Range of target[mm]
+            targetAngle_rad: Angle of target[rad]
+        Return:
+            insideFlag: Inside->True, Outside->False
+        """
         if targetRange_mm > self.maxRange_mm:
             return False
         
-        # ステップ2:扇形の境界角度より外側にあるか判定
-        # 左側の判定
         if targetAngle_rad > np.deg2rad(self.leftAngle_deg):
             return False
-        # 右側の判定
+
         if targetAngle_rad < np.deg2rad(self.rightAngle_deg):
             return False
         
-        # 上記の条件をクリアしたら検知範囲内にいると判定
         return True
 
     
