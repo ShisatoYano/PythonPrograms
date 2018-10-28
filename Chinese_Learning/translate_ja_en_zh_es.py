06d5d46a4ca6ec2efae691ca2445d3fc5e69ff63
LAPTOP-V8HODJUI\shisa <shisatoyano@gmail.com>
2018-10-28 17:39:05 +0900

    modified to save translation result as csv file.

--- Chinese_Learning/translate_ja_en_zh_es.py
+++ Chinese_Learning/translate_ja_en_zh_es.py
@@ -12,6 +12,7 @@ Output:
 
 import io, sys
 from py_translator import Translator
+import pandas as pd
 
 # set encoding utf-8 to prevent text garbling in Japanese or Chinese
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
@@ -19,10 +20,12 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 def to_japanese(translator_obj, input_phrase):
     translated_phrase = translator_obj.translate(input_phrase, dest='ja')
     print('In Japanese: %s' % translated_phrase.text)
+    return translated_phrase
 
 def to_english(translator_obj, input_phrase):
     translated_phrase = translator_obj.translate(input_phrase, dest='en')
     print('In English: %s' % translated_phrase.text)
+    return translated_phrase
 
 def to_chinese_simplified(translator_obj, input_phrase):
     translated_phrase = translator_obj.translate(input_phrase, dest='zh-CN')
@@ -30,10 +33,12 @@ def to_chinese_simplified(translator_obj, input_phrase):
         print('Pronounciation: %s' % translated_phrase.pronunciation)
     else:    
         print('In Simplified Chinese: %s Pronounciation: %s' % (translated_phrase.text, translated_phrase.pronunciation))
+    return translated_phrase
 
 def to_spanish(translator_obj, input_phrase):
     translated_phrase = translator_obj.translate(input_phrase, dest='es')
     print('In Spanish: %s' % translated_phrase.text)
+    return translated_phrase
 
 def translate(input_phrase):
     translator = Translator()
@@ -42,32 +47,54 @@ def translate(input_phrase):
     detect_result = translator.detect(input_phrase)
     language_type = detect_result.lang
 
+    # define data frame
+    trans_phrase_array = [''] * 5
+    colmuns_list = ['English', 'Japanese', 'Spanish', 'Chinese', 'Ch_Prns']
+
     print('Language type: %s' % language_type)
 
     # switch translation process depend on language type
     if 'ja' in language_type: # Japanese
         print('Input phrase is Japanese')
-        to_english(translator, input_phrase)
-        to_chinese_simplified(translator, input_phrase)
-        to_spanish(translator, input_phrase)
+        phrase_in_zh          = to_chinese_simplified(translator, input_phrase)
+        trans_phrase_array[0] = to_english(translator, input_phrase).text
+        trans_phrase_array[1] = input_phrase
+        trans_phrase_array[2] = to_spanish(translator, input_phrase).text
+        trans_phrase_array[3] = phrase_in_zh.text
+        trans_phrase_array[4] = phrase_in_zh.pronunciation
     elif 'zh-CN' in language_type: # Chinese(Simplified)
         print('Input phrase is Simplified Chinese')
-        to_chinese_simplified(translator, input_phrase)
-        to_japanese(translator, input_phrase)
-        to_english(translator, input_phrase)
-        to_spanish(translator, input_phrase)
+        phrase_in_zh          = to_chinese_simplified(translator, input_phrase)
+        trans_phrase_array[0] = to_english(translator, input_phrase).text
+        trans_phrase_array[1] = to_japanese(translator, input_phrase).text
+        trans_phrase_array[2] = to_spanish(translator, input_phrase).text
+        trans_phrase_array[3] = phrase_in_zh.text
+        trans_phrase_array[4] = phrase_in_zh.pronunciation
     elif 'en' in language_type: # English
         print('Input phrase is English')
-        to_japanese(translator, input_phrase)
-        to_chinese_simplified(translator, input_phrase)
-        to_spanish(translator, input_phrase)
+        phrase_in_zh          = to_chinese_simplified(translator, input_phrase)
+        trans_phrase_array[0] = input_phrase
+        trans_phrase_array[1] = to_japanese(translator, input_phrase).text
+        trans_phrase_array[2] = to_spanish(translator, input_phrase).text
+        trans_phrase_array[3] = phrase_in_zh.text
+        trans_phrase_array[4] = phrase_in_zh.pronunciation
     elif 'es' in language_type: # Spanish
         print('Input phrase is Spanish')
-        to_japanese(translator, input_phrase)
-        to_chinese_simplified(translator, input_phrase)
-        to_english(translator, input_phrase)
+        phrase_in_zh          = to_chinese_simplified(translator, input_phrase)
+        trans_phrase_array[0] = to_english(translator, input_phrase).text
+        trans_phrase_array[1] = to_japanese(translator, input_phrase).text
+        trans_phrase_array[2] = input_phrase
+        trans_phrase_array[3] = phrase_in_zh.text
+        trans_phrase_array[4] = phrase_in_zh.pronunciation
     else:
         print('Input phrase should be Japanese, English, Chinese or Spanish')
+    
+    # write in csv
+    # df_trans_phrase = pd.DataFrame(index=[], columns=colmuns_list)
+    df_trans_phrase = pd.read_csv('phrase_translation_database.csv', encoding='utf-8-sig', index_col=0)
+    sr_trans_phrase = pd.Series(trans_phrase_array, index=colmuns_list, name=input_phrase)
+    df_trans_phrase = df_trans_phrase.append(sr_trans_phrase)
+    df_trans_phrase.to_csv('phrase_translation_database.csv', encoding='utf-8-sig')
 
 if __name__ == '__main__':
     # get command line input
@@ -83,6 +110,10 @@ if __name__ == '__main__':
         print("The phrase should be enclosed in single or double quotations")
         input_phrase = ''
     
+    # read database csv file
+    # df_trans_db = pd.read_csv('phrase_translation_database.csv')
+    # print(df_trans_db)
+
     # translation
     if input_phrase:
         translate(input_phrase)
